f88c5143067e598fa6abba2eb843c48cd8c7bb76
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-07-14 16:46:19 -0400

    added boilerplate class syntax to Components

---
+++ src/PartCategories/PartCategories.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class PartCategories extends Component {
+    render() {
+        return(
+
+        );
+    }
+}
---
+++ src/PartList/PartList.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class PartList extends Component {
+    render() {
+        return(
+
+        );
+    }
+}
